27491dd695dec2ddcc611f969e430b3ea64febff
Utkarsh Kadam <kadamutkarsh21@gmail.com>
2024-07-14 22:21:35 +0530

    create suggest-messages api

---
+++ src/app/api/suggest-messages/route.ts
@@ -0,0 +1,34 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
+
+export async function POST(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'POST') {
+    res.setHeader('Allow', ['POST']);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+
+  try {
+    const prompt = "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
+
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    const response = await model.generateContent([{ text: prompt }]);
+
+    return Response.json(response);
+  } catch (error:any){
+    console.error('An unexpected error occurred:', error);
+    return Response.json(
+        {
+          message: "Unexpected error occurred",
+        },
+        {
+          status: 500,
+        },
+      );
+  }
+}
